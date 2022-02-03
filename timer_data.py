c043acfe8eb377f398608ce9b427b6f465149074
CandussoR <r.candusso@protonmail.com>
2022-02-03 10:31:01 +0100

    Suppression d'une fonction et requête sql.

--- timer_data.py
+++ timer_data.py
@@ -2,11 +2,10 @@ INSERT_TIMER_BEGINNING = 'INSERT INTO timer_data (task_id, date, time_beginning)
 
 UPDATE_TIMER_ENDING = 'UPDATE timer_data SET time_ending=(time(?)) WHERE time_ending IS NULL;'
 
-TIME_DIFFERENCE = '''SELECT strftime('%s',time_ending) - strftime('%s',time_beginning)
-                     FROM timer_data WHERE time_elapsed IS NULL;'''
-
-ADD_TIME_DIFFERENCE = '''UPDATE timer_data SET time_elapsed = time(?, 'unixepoch')
+ADD_TIME_DIFFERENCE = '''UPDATE timer_data
+                         SET time_elapsed = strftime('%s',time_ending) - strftime('%s',time_beginning)
                          WHERE time_elapsed IS NULL;'''
+
 
 def insert_beginning(connexion, task_at_hand_id, date, beginning_time):
     with connexion:
@@ -17,13 +16,7 @@ def update_ending(connexion, time_ending):
     with connexion:
         connexion.execute(UPDATE_TIMER_ENDING, [time_ending])
 
-def time_difference(connexion):
-    with connexion:
-        time_difference = connexion.execute(TIME_DIFFERENCE).fetchone()
-        return time_difference[0]
-
 def add_elapsed_time(connexion):
-    time_elapsed = time_difference(connexion)
     with connexion:
         connexion.execute(ADD_TIME_DIFFERENCE, [time_elapsed])
 
