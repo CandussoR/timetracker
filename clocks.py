b142ac1850767c0be788032b3b8ef916787419cf
Romain Candusso <r.candusso@protonmail.com>
2022-06-22 09:09:02 +0200

    Adds the possibility to interrupt the timer without exiting the app.

--- clocks.py
+++ clocks.py
@@ -2,12 +2,15 @@ import time
 
 def timer(t):
     for i in reversed(range(0, t)):
-        mins = t // 60
-        seconds = t % 60
-        time_format = '{:02d}:{:02d}'.format(mins, seconds)
-        print(time_format, end="\r")
-        time.sleep(1)
-        t -= 1
+        try:
+            mins = t // 60
+            seconds = t % 60
+            time_format = '{:02d}:{:02d}'.format(mins, seconds)
+            print(time_format, end="\r")
+            time.sleep(1)
+            t -= 1
+        except KeyboardInterrupt:
+            break
 
 def stopwatch(t=0):
     while True:
