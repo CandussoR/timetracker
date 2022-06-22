51c5389638d50fde296336deff1b6af058a8b525
Romain Candusso <r.candusso@protonmail.com>
2022-06-22 09:12:56 +0200

    Creating the possibility to add an old timer in the prompt, and the equivalent function in timer_data.

--- timer_data.py
+++ timer_data.py
@@ -1,5 +1,5 @@
 INSERT_TIMER_BEGINNING = '''INSERT INTO timer_data (task_id, date, time_beginning) 
-                            VALUES (?,date(?),time(?))'''
+                            VALUES (?,date(?),time(?));'''
 
 UPDATE_TIMER_ENDING = '''UPDATE timer_data 
                         SET time_ending=(time(?)) 
@@ -18,32 +18,37 @@ TIME_ELAPSED_PLUS_ONE_DAY = '''UPDATE timer_data
                          SET time_elapsed = time_elapsed + 86400
                          WHERE time_elapsed < 0;'''
 
+INSERT_PAST_TIMER = '''INSERT INTO timer_data (task_id, date, time_beginning, time_ending)
+                    VALUES (?,date(?),time(?),time(?));'''
 
 def insert_beginning(connexion, task_at_hand_id, date, beginning_time):
-    with connexion:
-        connexion.execute(INSERT_TIMER_BEGINNING, (task_at_hand_id, date,\
+    connexion.execute(INSERT_TIMER_BEGINNING, (task_at_hand_id, date,\
                                 beginning_time))
 
+def insert_old_timer(connexion, params):
+    connexion.execute(INSERT_PAST_TIMER, [*params])
+    update_time_elapsed(connexion)
 
 def update_row_at_ending(connexion, time):
     add_time_ending(connexion, time)
+    update_time_elapsed(connexion)
+
+def update_time_elapsed(connexion):
     add_elapsed_time(connexion)
     if time_elapsed_is_negative(connexion) :
         add_day_to_time_elapsed(connexion)
 
 def add_time_ending(connexion, time_ending):
-        with connexion:
-            connexion.execute(UPDATE_TIMER_ENDING, [time_ending])
+        connexion.execute(UPDATE_TIMER_ENDING, [time_ending])
 
 def add_elapsed_time(connexion):
-        with connexion:
-            connexion.execute(ADD_TIME_DIFFERENCE)
+        connexion.execute(ADD_TIME_DIFFERENCE)
 
 def time_elapsed_is_negative(connexion):
-        with connexion:
-            time_elapsed = connexion.execute(LAST_TIME_ELAPSED).fetchone()
-            return time_elapsed[1] < 0 
+        time_elapsed = connexion.execute(LAST_TIME_ELAPSED).fetchone()
+        return time_elapsed[1] < 0 
 
 def add_day_to_time_elapsed(connexion):
-        with connexion:
-            connexion.execute(TIME_ELAPSED_PLUS_ONE_DAY)
+        connexion.execute(TIME_ELAPSED_PLUS_ONE_DAY)
+
+    
