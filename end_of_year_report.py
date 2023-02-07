2072a8f254a573c0f9191f185c1b8f18ea26d263
CandussoR <r.candusso@protonmail.com>
2023-02-07 22:07:23 +0100

    Playing with matplotlib and making a pie

--- end_of_year_report.py
+++ end_of_year_report.py
@@ -4,6 +4,8 @@
 
 import sqlite_db as db
 from timer_stats import task_list
+from matplotlib.pyplot import pie, show, title
+from numpy import array
 
 connexion = db.connect('timer_data.db')
 
@@ -33,10 +35,18 @@ YEARLY_REPORT = f'''
     JOIN total_time tt ON td.task_name = tt.task_name
 '''
 
+task_array = []
+days_count_array = []
 for task in task_list(connexion) :
     task_name, days_done_count, total_time = connexion.execute(YEARLY_REPORT, [task[0]]).fetchone()
-    if days_done_count > 10:
-        print(f"{task_name} | {days_done_count} | {total_time}\n")
+    task_array.append(task_name)
+    days_count_array.append(int(days_done_count))
+    # if days_done_count > 10:
+    #     print(f"{task_name} | {days_done_count} | {total_time}\n")
 
+title("Days per task", loc="center")
+pie(array(days_count_array), labels=task_array, autopct="%.0f")
+# cf also https://stackoverflow.com/questions/6170246/how-do-i-use-matplotlib-autopct
+show()
 #TODO: extract data into a file
 #TODO: decide what the file should be, what the separators should be
