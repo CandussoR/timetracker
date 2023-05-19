f43cdf5e908e6e133109400b648252d6166e1be9
CandussoR <r.candusso@protonmail.com>
2023-05-19 11:52:55 +0200

    refactor: modified timers_and_pauses implementation, and added function docstrings

--- flows.py
+++ flows.py
@@ -1,9 +1,11 @@
 class Flow:
     def __init__(self, name: str, timers: list[int] | None = None, pauses: list[int] | None = None):
         self.name = name
-        self.timers, self.pauses = self._timers_and_pauses()
+        self.timers, self.pauses = self._timers_and_pauses(self._get_flow_input)
 
-    def _get_flow_input(self):
+    def _get_flow_input(self) -> list[list[str]]:
+        ''' Asking user input for time and pauses. Function abstracted for separation purpose.
+            Returns a list of a timer and a pause for each couple specified. '''
         timers_and_pauses = []
 
         while True: 
@@ -14,8 +16,9 @@ class Flow:
 
             timers_and_pauses.append(timer_pause.split('/'))
 
-    def _timers_and_pauses(self):
-        timers_pauses = self._get_flow_input()
+    def _timers_and_pauses(self, func ) -> tuple[list[int], list[int]] :
+        ''' Returns a tuple containing one list for the timers, and another one for the pauses.'''
+        timers_pauses = func()
         timers = [int(item[0]) for item in timers_pauses] 
         pauses = [int(item[1]) for item in timers_pauses]
         return timers, pauses
@@ -24,6 +27,9 @@ class Flow:
         pass
 
     def delete_flow(self):
+        pass
+
+    def execute_flow(self):
         pass
 
 if __name__ == '__main__':
