01b2d1502a472c66e4d5e667e99c7930e4416d60
CandussoR <r.candusso@protonmail.com>
2023-10-25 21:46:28 +0200

    feat: cleaner json dump

--- conf.py
+++ conf.py
@@ -9,6 +9,7 @@ class Config:
         conf = self._load()
         self.database = conf["database"]
         self.logs = conf["logs"]
+        self.log_path = conf["log_path"]
         self.timer_sound_path = conf["timer_sound_path"]
         self.flows = conf["flows"]
 
@@ -24,9 +25,8 @@ class Config:
         conf = {k : self.__dict__[k] for k in self.__dict__ if k != "filepath"}
 
         with open(self.filepath, 'w') as fw:
-            json.dump(conf, fw)
+            json.dump(conf, fw, indent=4, separators=(',', ': '))
 
 
 if __name__ == '__main__':
     conf = Config()
-    print(1)
