6a13b0ff0418464648ebbbecd58fac1a72f75305
CandussoR <r.candusso@protonmail.com>
2024-02-21 19:32:15 +0100

    feat: improving the tui stats facade.

--- src/tui/stats/stats_facade.py
+++ src/tui/stats/stats_facade.py
@@ -1,4 +1,5 @@
 from sqlite3 import Connection
+from typing import Literal
 
 from src.shared.repositories.stats_repository import SqliteStatRepository
 from src.shared.utils.format_time import format_time
@@ -7,26 +8,24 @@ def display_stats(connexion : Connection):
     repo = SqliteStatRepository(connexion=connexion)
     # Count number of timers and total time for different spans (day, week, year),
     # gives an average time per day and a maximum.
-    # today_timer = repo.timer_count('today')
-    # suffix = 's' if today_timer > 1 else ''
-    # print(f"Today : {today_timer} timer{suffix} ({repo.total_time('today')}).")
+    display_timer_count_and_time(repo, "today")
     
-    # for _, task, time, ratio in repo.get_task_time_ratio({"period": "day"}):
-    #     task_streak = repo.max_and_current_streaks(task)
-    #     max = "(max streak!)" if task_streak[0][1] == task_streak[0][0] else f"(max : {task_streak[0][0]})"
-    #     print(f"\t{task} : {format_time(time, 'hour')} ({ratio}%)")
-    #     print(f"\t\tCurrent streak : {task_streak[0][1]} {max}")
+    for _, task, time, ratio in repo.get_task_time_ratio({"period": "day"}):
+        if time is not None:
+            task_streak = repo.max_and_current_streaks(task)
+            max = "(max streak!)" if task_streak[0][1] == task_streak[0][0] else f"(max : {task_streak[0][0]})"
+            print(f"\t{task} : {format_time(time, 'hour')} ({ratio}%)")
+            print(f"\t\tCurrent streak : {task_streak[0][1]} {max}")
 
-    # print(f"\nThis year's average day : {repo.average_day_this_year()}")
+    print(f"\nThis year's average day : {repo.average_day_this_year()}")
 
-    # max_stat = repo.max_in_a_day()
-    # print(f"\nYour maximum : {max_stat[1]} ({max_stat[0]}).\n")
+    max_stat = repo.max_in_a_day()
+    print(f"\nYour maximum : {max_stat[1]} ({max_stat[0]}).")
 
-    # print(f"This week : { repo.timer_count('week') } timer{suffix} ({ repo.total_time('week') }).\n")
-
-    # print(f"This year : {repo.timer_count('year')} timer{suffix} ({repo.total_time('year')}).\n")
-
-    more = input("See every task max streak (y) ?\n\t>> ")
+    display_timer_count_and_time(repo, "week")
+    display_timer_count_and_time(repo, "year")
+ 
+    more = input("\nSee every task max streak (y) ?\n\t>> ")
     if more == 'y':
         for task, streak, current_streak in repo.all_task_streaks():
             suffix = f'(current : {current_streak})' if current_streak != 1 else ''
@@ -39,3 +38,28 @@ def display_stats(connexion : Connection):
         time_per_week = repo.past_weeks(number_of_weeks)
         for monday, sunday, tpw in time_per_week:
             print(f"Week from Monday {monday} to Sunday {sunday} : \n\t Total time : {tpw}")
+
+
+def display_timer_count_and_time(repo : SqliteStatRepository, period : Literal["today", "week", "year"]) :
+    match(period):
+        case "today":
+            range = "This day"
+        case "week":
+            range = "This week"
+        case "year":
+            range = "This year"
+        case _:
+            raise Exception("Wrong argument.")    
+            
+    timer_count = repo.timer_count(period)
+    suffix = 's' if timer_count > 1 else ''
+    total_time = repo.total_time(period)
+    newline = "\n" if period != "today" else ''
+    if timer_count == 0:
+        print(f"{newline}{range} : {timer_count} timer{suffix}.")
+    elif total_time is None and timer_count == 1:
+       print(f"{newline}{range} : {timer_count} timer{suffix} (ongoing).") 
+    else:
+        formatted = format_time(total_time, 'hour') if period != "year" else format_time(total_time, 'day')
+        print(f"{newline}{range} : {timer_count} timer{suffix} ({formatted}).")
+        
