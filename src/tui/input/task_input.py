dcfd91a51910326f584ccdce084cac53685cf177
Romain Candusso <r.candusso@protonmail.com>
2024-10-13 18:27:21 +0200

    fix: terminal loop bugs and formatting

--- src/tui/input/task_input.py
+++ src/tui/input/task_input.py
@@ -11,32 +11,32 @@ def get_task_rank_from_input(repository : SqliteTaskRepository, task_input : str
             # Sends TypeError if None.
             return repository.fetch_task_id(task)
         except TypeError:
-            add_task = input("The task doesn't exist. Do you want to add it ? (Y/N) > ")
+            add_task = input("\tThe task doesn't exist. Do you want to add it ? (Y/N) > ")
             
             if add_task.upper().strip() == "Y":
                 _,_,guid = repository.insert_new_task(task)
                 repository.connexion.commit()
                 return repository.get_id_from_guid(guid)
             elif add_task.upper().strip() == "N":
-                task_input = input("Enter the task name again : > ")
+                task_input = input("\tEnter the task name again : > ")
                 continue
             else :
-                print("Enter Y for yes or N for no.")
+                print("\tEnter Y for yes or N for no.")
         
 
 def task_string_input() -> str:
     while True:
         try:
-            task_input = input("Enter a task. > ")
+            task_input = input("\tEnter a task. > ")
             if int(task_input):
-                print("Task name must be a string.")
+                print("\tTask name must be a string.")
         except ValueError :
             return task_input
             
 
 def parse_input(task_input : str) -> Task:
     if re.search(r'\W', task_input):
-        task, subtask = re.split('\W', task_input, 1)
+        task, subtask = re.split(r'\W', task_input, 1)
         return Task(task_name = task, subtask = subtask)    
     else:
         return Task(task_name = task_input)
