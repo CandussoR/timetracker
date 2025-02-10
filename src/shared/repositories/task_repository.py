ad90984fe32e96f8e82d9568617678e4474a043d
Romain Candusso <r.candusso@protonmail.com>
2025-02-10 21:47:49 +0100

    fix : ensures nullity of subtask when requesting task only

--- src/shared/repositories/task_repository.py
+++ src/shared/repositories/task_repository.py
@@ -1,7 +1,8 @@
-from sqlite3 import Connection
+from sqlite3 import Connection, IntegrityError
 from typing import Optional
 from flask import g, jsonify
 from src.shared.database.sqlite_db import connect
+from src.shared.exceptions.unique_constraint import UniqueConstraintError
 from src.shared.models.task import Task
 
 class SqliteTaskRepository():
@@ -19,20 +20,21 @@ class SqliteTaskRepository():
 
 
     def fetch_task_id(self, task : Task) -> int :
-        '''Method used only in the offline terminal version.'''
-        # Select the id for selection in the timer_data table.
         if task.subtask :
             query = 'SELECT id FROM tasks WHERE task_name= :task_name AND subtask= :subtask ;'
         else :
-            query = 'SELECT id FROM tasks WHERE task_name= :task_name;'        
-        return self.connexion.execute(query,task.__dict__).fetchone()[0]
+            query = 'SELECT id FROM tasks WHERE task_name= :task_name AND subtask IS NULL;'       
+        result = self.connexion.execute(query,task.__dict__).fetchone()
+        # Just preventing Nonetype exceptions
+        return result if not result else result[0]
 
 
     def insert_new_task(self, task : Task):
-            query = '''INSERT INTO tasks (task_name, subtask, guid) VALUES (:task_name, :subtask, :guid) 
-                                          RETURNING task_name, subtask, guid;'''
-            return self.connexion.execute(query, task.__dict__).fetchone()
+        query = '''INSERT INTO tasks (task_name, subtask, guid) VALUES (:task_name, :subtask, :guid) 
+                                        RETURNING task_name, subtask, guid;'''
+        return self.connexion.execute(query, task.__dict__).fetchone()
 
+            
 
     def update(self, task : Task) -> tuple:
         '''Returns (task_name, subtask, guid).'''
