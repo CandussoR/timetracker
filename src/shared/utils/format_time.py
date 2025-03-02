110b69b8daa83509563b3ff0116a0aae1b624bc0
Romain Candusso <r.candusso@protonmail.com>
2025-03-02 23:23:37 +0100

    fix : format time
    Week was resetting to 00 hours if superior to 24 hours. I don't know what change created this bugg which didn't exist before the last commits, but it works now.

--- src/shared/utils/format_time.py
+++ src/shared/utils/format_time.py
@@ -4,8 +4,10 @@ def format_time(time: float | int, max_unit: Literal["day", "hour"] = "hour", sp
     '''Returns a string in format (00:)00:00:00, unless split is True (generally for the front).'''
     result = []
     if max_unit == "day" and time >= 86400:
-        result.append(int(time // 86400))
-    result.extend([int((time % 86400) // 3600), int((time % 3600) // 60), int(time % 60)])
+        result.extend([int(time // 86400), int((time % 86400) // 3600)])
+    else :
+        result.append(int(time//3600))
+    result.extend([int((time % 3600) // 60), int(time % 60)])
     assert len(result) >= 3
     # Padding on numbers if needed (< 10)
     result = list(map(lambda x : f"{x:02d}", result))
